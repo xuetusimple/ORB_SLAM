a99d205498fbc86b1b26e7aed8decb10f38903d4
Xue Mei <xue.mei@tusimple.ai>
2017-12-17 17:19:09 -0800

    Update Initializer.cc

--- src/Initializer.cc
+++ src/Initializer.cc
@@ -1,25 +1,4 @@
-/**
-* This file is part of ORB-SLAM.
-*
-* Copyright (C) 2014 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
-* For more information see <http://webdiis.unizar.es/~raulmur/orbslam/>
-*
-* ORB-SLAM is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ORB-SLAM is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with ORB-SLAM. If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include "Initializer.h"
-
 #include "Thirdparty/DBoW2/DUtils/Random.h"
 
 #include "Optimizer.h"
